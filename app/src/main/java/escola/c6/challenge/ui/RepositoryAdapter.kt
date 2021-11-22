306ff9021d2f5a9f149eb462a34c934892658bfc
Lucas Campos <lucasgcampos.contato@gmail.com>
2021-11-22 00:37:57 -0300

    Custom view

--- app/src/main/java/escola/c6/challenge/ui/RepositoryAdapter.kt
+++ app/src/main/java/escola/c6/challenge/ui/RepositoryAdapter.kt
@@ -2,12 +2,11 @@ package escola.c6.challenge.ui
 
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import escola.c6.challenge.R
-import escola.c6.challenge.extension.getString
 import escola.c6.challenge.extension.inflate
 import escola.c6.challenge.network.model.RepositoryResponse
+import escola.c6.challenge.ui.widget.RepositoryView
 
 private const val JAVA_VIEW_TYPE = 42
 private const val REPOSITORY_VIEW_TYPE = 7
@@ -30,7 +29,7 @@ class RepositoryAdapter(
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
         if (viewType == REPOSITORY_VIEW_TYPE) {
-            RepositoryViewHolder(parent.inflate(R.layout.item_repository))
+            RepositoryViewHolder(RepositoryView(parent.context))
         } else {
             JavaViewHolder(parent.inflate(R.layout.item_java))
         }
@@ -48,15 +47,10 @@ class RepositoryAdapter(
 
 class JavaViewHolder(view: View) : RecyclerView.ViewHolder(view)
 
-class RepositoryViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-    private val author = view.findViewById<TextView>(R.id.txt_author)
-    private val name = view.findViewById<TextView>(R.id.txt_repository_name)
-    private val description = view.findViewById<TextView>(R.id.txt_repository_description)
+class RepositoryViewHolder(private val view: RepositoryView) : RecyclerView.ViewHolder(view) {
 
     fun bind(repository: RepositoryResponse) {
-        name.text = repository.name
-        author.text = getString(R.string.item_author_label, repository.owner.login)
-        description.text = getString(R.string.item_repository_description_label, repository.description)
+        view.setup(repository)
     }
 }
 
---
+++ app/src/main/java/escola/c6/challenge/ui/widget/RepositoryView.kt
@@ -0,0 +1,30 @@
+package escola.c6.challenge.ui.widget
+
+import android.content.Context
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.setPadding
+import escola.c6.challenge.R
+import escola.c6.challenge.extension.inflate
+import escola.c6.challenge.network.model.RepositoryResponse
+
+class RepositoryView(context: Context) : ConstraintLayout(context) {
+
+    private val author by lazy { findViewById<TextView>(R.id.txt_author) }
+    private val name by lazy { findViewById<TextView>(R.id.txt_repository_name) }
+    private val description by lazy { findViewById<TextView>(R.id.txt_repository_description) }
+
+    init {
+        inflate(R.layout.item_repository)
+        layoutParams = LayoutParams(MATCH_PARENT, WRAP_CONTENT)
+        setPadding(resources.getDimensionPixelSize(R.dimen.spacing_medium))
+    }
+
+    fun setup(repository: RepositoryResponse) {
+        name.text = repository.name
+        author.text = context.getString(R.string.item_author_label, repository.owner.login)
+        description.text = context.getString(R.string.item_repository_description_label, repository.description)
+    }
+}
